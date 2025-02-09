3cdd9fb8c4469832b93af9269bfc68d9a9768969
gee12 <yagee12@gmail.com>
2025-02-09 15:50:09 +0300

    Подправил, чтобы если запись не в избранном, то в xml вообще не записывался аттрибут "favor"

--- app/src/models/recordTable/Record.cpp
+++ app/src/models/recordTable/Record.cpp
@@ -114,8 +114,14 @@ void Record::exportDataToStreamWriter(QXmlStreamWriter *xmlWriter) const
     QString currentFieldName=availableFieldList.at(j);
 
     // Устанавливается значение поля как атрибут записи
-    if(isNaturalFieldExists(currentFieldName))
-      xmlWriter->writeAttribute(currentFieldName, getNaturalFieldSource(currentFieldName));
+    if(isNaturalFieldExists(currentFieldName)) {
+      QString fieldValue = getNaturalFieldSource(currentFieldName);
+      // Если это поле "favor", тогда проверяем, что значение не пусто,
+      // чтобы не "захламлять" дерево пустыми аттрибутами
+      if (currentFieldName != "favor" || !fieldValue.isEmpty()) {
+        xmlWriter->writeAttribute(currentFieldName, fieldValue);
+      }
+    }
   }
 
   // К элементу записи прикрепляется элемент таблицы приаттаченных файлов, если таковые есть
